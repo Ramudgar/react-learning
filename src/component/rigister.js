9e6606a1308884a69e6acdadb0591ef048367a8d
Ramudgar <ramudgary52@gmail.com>
2022-06-06 13:44:58 +0545

    radio and dropdown

--- src/component/rigister.js
+++ src/component/rigister.js
@@ -6,6 +6,8 @@ const Register = () => {
     const [password,setPassword]=useState("");
 
     const [confirmPassword,setConfirmPassword]=useState("");
+    const [country,setCountry]=useState("");
+    const [gender,setGender]=useState("");
 
     return(
         <>
@@ -20,6 +22,43 @@ const Register = () => {
        <p>{email}</p>
         <input type="text" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
 
+        
+        <div className="form-group container">
+            <label>{gender}</label>
+            <label>Gender</label>
+          <label>  <input
+            type="radio"
+            name="gen"
+            value="Male"
+            onChange={(e)=>setGender(e.target.value)}/>
+            Male</label>
+            <label> 
+            <input
+            type="radio"
+            name="gen"
+            value="FeMale"
+            onChange={(e)=>setGender(e.target.value)}/>Female   </label>
+          
+
+            
+              
+
+           
+            </div>
+
+       <div className="form-group">
+        <label>{country}</label><br></br>
+        <label className="">Select your country</label>
+        <select onChange={(e)=>setCountry(e.target.value)}>
+            <option >nepal</option>
+            <option >india</option>
+            <option >china</option>
+            <option >usa</option>
+        </select>
+
+           </div>
+        
+
         <label>Password</label>
         <p>{password}</p>
         <input type="password" className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
@@ -28,6 +67,8 @@ const Register = () => {
         <input type="password" className="form-control" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}></input>
 
         <button className="btn btn-primary">Register</button>
+
+
     </div>
     </div>
         </>
